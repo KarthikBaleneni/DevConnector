f4f471b802f0992885efb9178e693270da1f9e71
KarthikBaleneni <kartiikeyabaleneni@gmail.com>
2025-10-21 15:07:00 +0530

    Express validator usage

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const { check, query, validationResult } = require('express-validator');
 const router = express.Router();
 
-//@route  GET api/users
-//desc    Test Route
+//@route  POST api/users
+//desc    registration Logic
 //access  Public
 
-router.get('/', (req,res)=> res.send('User Route'));
+//router.get('/',  (req,res)=> res.send('User Route'));
+router.post('/', [
+    check('name', 'Name is required').not().notEmpty(),
+    check('email', 'Valid Email ID is required').isEmail(),
+    check('password', 'Enter a password with length of minimum six characters').isLength({min: 6})
+
+] ,(req,res)=> {
+    const errros = validationResult(req);
+    if(!errros.isEmpty()){
+        return res.status(400).json({errros: errros.array()})
+    } 
+    res.send('User Route') }) ;
 
 module.exports = router;
