f1b20db056fcdb1e046a3d6df0709dbf82676d12
KarthikBaleneni <kartiikeyabaleneni@gmail.com>
2025-10-24 13:29:52 +0530

    react router setup and initial navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-
 import './App.css';
 import {Fragment} from "react";
+import {BrowserRouter as Router, useRoutes} from "react-router-dom";
+import NavBar from "./components/layouts/NavBar";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Landing from "./components/layouts/Landing";
 
+//const cors = require('cors');
+//app.use(cors());
+const AppRoutes = () => {
+    return useRoutes([
+       { path: "/", element: <Landing /> },
+        {path: "/register", element: <Register/>},
+        {path: "/login", element: <Login/>},
+    ]);
+  };
 const  App = ()=> {
   return (
   <Fragment>
-    <h1>Hello Mern Stack</h1>
+    <Router>
+   <NavBar/>
+    <AppRoutes />
+  </Router>
   </Fragment>
   )
 }
